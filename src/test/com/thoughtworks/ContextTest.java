1c8c77f6782ae1a0e05d2d891c56e061e97fca6b
Zhang JiuKun <jiukunz@gmail.com>
2013-04-02 17:38:00 +0800

    first commit. Add build file and write a fake test file to check the env

---
+++ src/test/com/thoughtworks/ContextTest.java
@@ -0,0 +1,15 @@
+package com.thoughtworks;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ContextTest {
+
+    @Test
+    public void justForTest() {
+        assertThat(1,is(1));
+    }
+}
