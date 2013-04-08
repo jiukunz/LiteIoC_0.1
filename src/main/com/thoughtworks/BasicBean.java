7ac935e36568e9a162f202d2e9b806091a5d26f2
Zhang JiuKun <jiukunz@gmail.com>
2013-04-08 20:15:52 +0800

    add vallue to basic bean

--- src/main/com/thoughtworks/BasicBean.java
+++ src/main/com/thoughtworks/BasicBean.java
@@ -4,9 +4,10 @@ public class BasicBean implements Bean {
 
     private String name;
     private Class clazz;
+    private Object value;
 
     @Override
     public Object toObject() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return value;
     }
 }
