fd25d6f31bb7a7ddce27de489ccbaf45ce502d54
chenqutw <cqu@thoughtworks.com>
2013-04-10 10:21:54 +0800

    a little clean up

--- src/main/com/thoughtworks/XMLHandler.java
+++ src/main/com/thoughtworks/XMLHandler.java
@@ -16,6 +16,13 @@ public class XMLHandler extends DefaultHandler {
     private Map<String, Bean> constructParams = newHashMap();
     private Stack<Bean> beans = new Stack<Bean>();
     private Context context;
+    private final String BEAN_ELEMENT = "bean";
+    private final String PROPERTY_ELEMENT = "property";
+    private final String CONSTRUCT_ELEMENT = "construct";
+    private final String ATTR_VALUE = "value";
+    private final String ATTR_NAME = "name";
+    private final String ATTR_CLASS = "class";
+    private final String ATTR_REF = "ref";
 
     public XMLHandler(Context context) {
         this.context = context;
@@ -23,55 +30,80 @@ public class XMLHandler extends DefaultHandler {
 
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        String beanName = attributes.getValue("name");
-        String value = attributes.getValue("value");
-        String classString = attributes.getValue("class");
-        String ref = attributes.getValue("ref");
+        String beanName = attributes.getValue(ATTR_NAME);
+        String value = attributes.getValue(ATTR_VALUE);
+        String classString = attributes.getValue(ATTR_CLASS);
+        String ref = attributes.getValue(ATTR_REF);
 
-        if (qName.equals("bean") || qName.equals("property") || qName.equals("construct")) {
-            if (ref != null) {
-                currentBean = context.getBeans().get(beanName);
-            } else {
-                currentBean = new Bean();
-                if (value != null) {
-                    currentBean.setValue(value);
-                }
-                if (classString != null) {
-                    try {
-                        currentBean.setClazz(Class.forName(classString));
-                    } catch (ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-                currentBean.setName(beanName);
-            }
-            beans.push(currentBean);
+        if (!isInNormalElement(qName)) {
+            return;
         }
+
+        if (ref != null) {
+            currentBean = context.getBeans().get(beanName);
+        } else
+        {
+            currentBean = new Bean();
+            currentBean.setValue(value);
+
+            if (classString != null) {
+                try {
+                    currentBean.setClazz(Class.forName(classString));
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            currentBean.setName(beanName);
+        }
+
+        beans.push(currentBean);
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
-        if (qName.equals("bean")) {
-            currentBean = beans.pop();
+
+        if(!isInNormalElement(qName)) {
+            return;
+        }
+
+        currentBean = beans.pop();
+
+        if (isInBean(qName)) {
             if (currentBean.getValue() == null) {
+
                 currentBean.setParams(beanParams);
                 currentBean.setConstructParams(constructParams);
+
                 beanParams.clear();
                 constructParams.clear();
             }
             context.getBeans().put(currentBean.getName(), currentBean);
         }
 
-        if (qName.equals("property")) {
-            currentBean = beans.pop();
+        if (isInProperty(qName)) {
             beanParams.put(currentBean.getName(), currentBean);
         }
 
-        if (qName.equals("construct")) {
-            currentBean = beans.pop();
+        if (isInConstruct(qName)) {
             constructParams.put(currentBean.getName(), currentBean);
         }
 
+    }
+
+    private boolean isInNormalElement(String qName) {
+        return isInBean(qName) || isInProperty(qName) || isInConstruct(qName);
+    }
+
+    private boolean isInConstruct(String qName) {
+        return qName.equals(CONSTRUCT_ELEMENT);
+    }
+
+    private boolean isInProperty(String qName) {
+        return qName.equals(PROPERTY_ELEMENT);
+    }
+
+    private boolean isInBean(String qName) {
+        return qName.equals(BEAN_ELEMENT);
     }
 
 }
